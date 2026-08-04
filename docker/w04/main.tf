b9afd59ec150974ceb95d7ae6a82d933dc724a50
dennisn <dennis.nguyen.06@gmail.com>
2026-08-04 10:57:51 +1000

    for_each

--- docker/w04/main.tf
+++ docker/w04/main.tf
@@ -10,18 +10,54 @@ resource "docker_image" "nginx" {
   name = "nginx:alpine"
 }
 
-resource "docker_container" "web" {
-  count = length(var.web_ports)
+# resource "docker_container" "web" {
+#   count = length(var.web_ports)
 
-  name  = "w04-web-${count.index + 1}"
+#   name  = "w04-web-${count.index + 1}"
+#   image = docker_image.nginx.image_id
+
+#   ports {
+#     internal = 80
+#     external = var.web_ports[count.index]
+#   }
+
+#   networks_advanced {
+#     name = docker_network.application.name
+#   }
+# }
+
+resource "docker_container" "web" {
+  for_each = var.web_containers
+
+  name  = "w04-web-${each.key}-${each.value.test_label}"
   image = docker_image.nginx.image_id
 
   ports {
     internal = 80
-    external = var.web_ports[count.index]
+    external = each.value.external_port
+  }
+
+  labels {
+    label = "test_label"
+    value = each.value.test_label
   }
 
   networks_advanced {
     name = docker_network.application.name
   }
+}
+
+moved {
+  from = docker_container.web[0]
+  to   = docker_container.web["frontend"]
+}
+
+moved {
+  from = docker_container.web[1]
+  to   = docker_container.web["admin"]
+}
+
+moved {
+  from = docker_container.web[2]
+  to   = docker_container.web["report"]
 }
--- docker/w04/output.tf
+++ docker/w04/output.tf
@@ -1,3 +1,12 @@
 output "web_container_names" {
-  value = docker_container.web[*].name
+  #value = docker_container.web.name
+  value = [for instance in docker_container.web : instance.name]
+}
+
+output "web_container_names_name" {
+  value = { for key, instance in docker_container.web : key => instance.name }
+}
+
+output "web_container_test_label" {
+  value = [for instance in docker_container.web : instance.labels ]
 }
